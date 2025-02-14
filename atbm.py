b614cc398cf06b5edf4df188ce291c4855e1d80a
TruongTuyetTrinh <126968974+TruongTuyetTrinh@users.noreply.github.com>
2025-02-14 20:50:26 +0700

    Add files via upload

---
+++ atbm.py
@@ -0,0 +1,14 @@
+
+L = ['T', 'R', 'U', 'O', 'N', 'G', 'T', 'U', 'Y', 'E', 'T', 'T', 'R', 'I', 'N', 'H']
+k = 14
+ciphertext = []
+
+for char in L:
+    if 'A' <= char <= 'Z':
+        new_char = chr((ord(char) - ord('A') + k) % 26 + ord('A'))
+        ciphertext.append(new_char)
+    else:
+        ciphertext.append(char)
+
+ciphertext = ''.join(ciphertext)
+print("Ciphertext:", ciphertext)
